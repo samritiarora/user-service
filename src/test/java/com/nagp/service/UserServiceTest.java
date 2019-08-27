eca1e75c40e5e97f4bbcd991213d3615d7c99be5
Samriti Arora <samriti.arora@nagarro.com>
2019-08-27 15:32:51 +0530

    Adding dummy test and jacoco
    Change-Id: I70b93a4058cac419562de34cad112506b4b3f11e

---
+++ src/test/java/com/nagp/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.nagp.service;
+
+
+import org.junit.Test;
+
+public class UserServiceTest
+{
+    @Test
+    public void testCreateInventoryForSomeInventoryPositive()
+    {
+    }
+
+}
